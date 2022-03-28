975483770e55f6d4ac40126d08270d951470e85f
Faker <Faker@apple.com>
2022-03-28 18:01:33 +0800

    Update jd_identicalnew.py

--- jd_identicalnew.py
+++ jd_identicalnew.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "okyyds_yyds_master")
+sub_str = os.getenv("RES_SUB", "shufflewzc_faker2")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
