0f7db5e8305940bdb41741a0119945ecfaebcdc2
Faker <Faker@apple.com>
2022-06-29 11:13:01 +0800

    update

--- jd_queryRedpacket.ts
+++ jd_queryRedpacket.ts
@@ -2,11 +2,27 @@ import {JDHelloWorld, User} from "./TS_JDHelloWorld";
 
 class Jd_queryRedpacket extends JDHelloWorld {
   constructor() {
-    super();
+    super("京东红包");
   }
 
   async init() {
     await this.run(new Jd_queryRedpacket())
+  }
+
+  add(arg1: number, arg2: number) {
+    let r1, r2, m
+    try {
+      r1 = arg1.toString().split('.')[1].length
+    } catch (e) {
+      r1 = 0
+    }
+    try {
+      r2 = arg2.toString().split('.')[1].length
+    } catch (e) {
+      r2 = 0
+    }
+    m = Math.pow(10, Math.max(r1, r2))
+    return parseFloat(((arg1 * m + arg2 * m) / m).toFixed(2))
   }
 
   async main(user: User) {
@@ -16,18 +32,23 @@ class Jd_queryRedpacket extends JDHelloWorld {
       "Cookie": user.cookie,
       'User-Agent': user.UserAgent
     })
-    let day: number = new Date().getDay(), jdRed: number = 0, jdRedExp: number = 0
+    let day: number = new Date().getDay(), jdRed: number = 0, jdRedExp: number = 0, jsRed: number = 0, jsRedExp: number = 0
     for (let j of res.data.useRedInfo?.redList || []) {
+      console.log(j)
       if (j.orgLimitStr.includes('京喜')) {
       } else if (j.activityName.includes('极速版')) {
+        jsRed += j.balance
+        if (new Date(j.endTime * 1000).getDay() === day)
+          jsRedExp = this.add(jsRedExp, j.balance)
       } else if (j.orgLimitStr.includes('京东健康')) {
       } else {
-        jdRed = add(jdRed, j.balance)
+        jdRed = this.add(jdRed, j.balance)
         if (new Date(j.endTime * 1000).getDay() === day)
-          jdRedExp = add(jdRedExp, j.balance)
+          jdRedExp = this.add(jdRedExp, j.balance)
       }
     }
-    console.log(jdRed, '  今日过期：', jdRedExp)
+    console.log('京东', jdRed, '  今日过期：', jdRedExp)
+    console.log('极速', jsRed, '  今日过期：', jsRedExp)
     let msg = `【账号】  ${user.UserName}\n京东红包  ${jdRed}\n今日过期  ${jdRedExp}\n\n`
     return {
       msg: msg
@@ -35,20 +56,4 @@ class Jd_queryRedpacket extends JDHelloWorld {
   }
 }
 
-new Jd_queryRedpacket().init().then().catch()
-
-function add(arg1: number, arg2: number) {
-  let r1, r2, m
-  try {
-    r1 = arg1.toString().split('.')[1].length
-  } catch (e) {
-    r1 = 0
-  }
-  try {
-    r2 = arg2.toString().split('.')[1].length
-  } catch (e) {
-    r2 = 0
-  }
-  m = Math.pow(10, Math.max(r1, r2))
-  return parseFloat(((arg1 * m + arg2 * m) / m).toFixed(2))
-}
+new Jd_queryRedpacket().init().then()
