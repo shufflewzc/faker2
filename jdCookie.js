940d90e167aa3f34a790da82ecc47c3dd680b715
Faker <Faker@apple.com>
2022-11-21 16:39:11 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -88,7 +88,7 @@ function SetShareCodesEnv(nameConfig = "", envName = "") {
     let shareCodesStr = shareCodes.join('&')
     process.env[envName] = shareCodesStr
 
-    console.info(`【风之凌殇】 友情提示：为避免ck超过45以上时，互助码环境变量过大而导致调用一些系统命令（如date/cat）时报 Argument list too long，改为在nodejs中设置 ${nameConfig} 的 互助码环境变量 ${envName}，共计 ${totalCodeCount} 组互助码，总大小为 ${shareCodesStr.length}`)
+    console.info(`faker友情提示：为避免ck超过45以上时，互助码环境变量过大而导致调用一些系统命令（如date/cat）时报 Argument list too long，改为在nodejs中设置 ${nameConfig} 的 互助码环境变量 ${envName}，共计 ${totalCodeCount} 组互助码，总大小为 ${shareCodesStr.length}`)
 }
 
 // 若在task_before.sh 中设置了要设置互助码环境变量的活动名称和环境变量名称信息，则在nodejs中处理，供活动使用
