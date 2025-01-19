cc17878bc642d541ea23caa697c567b9b90b1332
Faker <Faker@apple.com>
2025-01-19 13:49:13 +0800

    1

--- function/ql.js
+++ function/ql.js
@@ -4,147 +4,149 @@ const got = require('got');
 require('dotenv').config();
 const { readFile } = require('fs/promises');
 const path = require('path');
-
-const qlDir = '/ql';
 const fs = require('fs');
+let Fileexists280 = fs.existsSync('/ql/data/db/keyv.sqlite');
 let Fileexists = fs.existsSync('/ql/data/config/auth.json');
-let authFile="";
-if (Fileexists) 
-	authFile="/ql/data/config/auth.json"
+let authFile = "";
+if (Fileexists280)
+    authFile = "/ql/data/db/keyv.sqlite"
+else if (Fileexists)
+    authFile = "/ql/data/config/auth.json"
 else
-	authFile="/ql/config/auth.json"
-//const authFile = path.join(qlDir, 'config/auth.json');
+    authFile = "/ql/config/auth.json"
+
 
 const api = got.extend({
-  prefixUrl: 'http://127.0.0.1:5600',
-  retry: { limit: 0 },
+    prefixUrl: 'http://127.0.0.1:5600',
+    retry: { limit: 0 },
 });
 
 async function getToken() {
-  const authConfig = JSON.parse(await readFile(authFile));
-  return authConfig.token;
+    const authConfig = await readFile(authFile);
+    // console.log(authConfig.toString().match(/"token":"(.*?)",/)[1])
+    return authConfig.toString().match(/"token":"(.*?)",/)[1];
 }
-
-module.exports.getEnvs = async () => {  
-  const token = await getToken();
-  const body = await api({
-    url: 'api/envs',
-    searchParams: {
-      searchValue: 'JD_COOKIE',
-      t: Date.now(),
-    },
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-  }).json();
-  return body.data;
+// getToken()
+module.exports.getEnvs = async () => {
+    const token = await getToken();
+    const body = await api({
+        url: 'api/envs',
+        searchParams: {
+            searchValue: 'JD_COOKIE',
+            t: Date.now(),
+        },
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+        },
+    }).json();
+    return body.data;
 };
 
 module.exports.getEnvsCount = async () => {
-  const data = await this.getEnvs();
-  return data.length;
+    const data = await this.getEnvs();
+    return data.length;
 };
 
 module.exports.addEnv = async (cookie, remarks) => {
-  const token = await getToken();
-  const body = await api({
-    method: 'post',
-    url: 'api/envs',
-    params: { t: Date.now() },
-    json: [{
-      name: 'JD_COOKIE',
-      value: cookie,
-      remarks,
-    }],
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  }).json();
-  return body;
+    const token = await getToken();
+    const body = await api({
+        method: 'post',
+        url: 'api/envs',
+        params: { t: Date.now() },
+        json: [{
+            name: 'JD_COOKIE',
+            value: cookie,
+            remarks,
+        }],
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+    }).json();
+    return body;
 };
 
 module.exports.updateEnv = async (cookie, eid, remarks) => {
-  const token = await getToken();
-  const body = await api({
-    method: 'put',
-    url: 'api/envs',
-    params: { t: Date.now() },
-    json: {
-      name: 'JD_COOKIE',
-      value: cookie,
-      _id: eid,
-      remarks,
-    },
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  }).json();
-  return body;
+    const token = await getToken();
+    const body = await api({
+        method: 'put',
+        url: 'api/envs',
+        params: { t: Date.now() },
+        json: {
+            name: 'JD_COOKIE',
+            value: cookie,
+            _id: eid,
+            remarks,
+        },
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+    }).json();
+    return body;
 };
 
 module.exports.updateEnv11 = async (cookie, eid, remarks) => {
-  const token = await getToken();
-  const body = await api({
-    method: 'put',
-    url: 'api/envs',
-    params: { t: Date.now() },
-    json: {
-      name: 'JD_COOKIE',
-      value: cookie,
-      id: eid,
-      remarks,
-    },
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  }).json();
-  return body;
+    const token = await getToken();
+    const body = await api({
+        method: 'put',
+        url: 'api/envs',
+        params: { t: Date.now() },
+        json: {
+            name: 'JD_COOKIE',
+            value: cookie,
+            id: eid,
+            remarks,
+        },
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+    }).json();
+    return body;
 };
 
 module.exports.DisableCk = async (eid) => {
-  const token = await getToken();
-  const body = await api({
-    method: 'put',
-    url: 'api/envs/disable',
-    params: { t: Date.now() },	
-    body: JSON.stringify([eid]),
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  }).json();
-  return body;
+    const token = await getToken();
+    const body = await api({
+        method: 'put',
+        url: 'api/envs/disable',
+        params: { t: Date.now() },
+        body: JSON.stringify([eid]),
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+    }).json();
+    return body;
 };
 
 module.exports.EnableCk = async (eid) => {
-  const token = await getToken();
-  const body = await api({
-    method: 'put',
-    url: 'api/envs/enable',
-    params: { t: Date.now() },	
-    body: JSON.stringify([eid]),
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  }).json();
-  return body;
+    const token = await getToken();
+    const body = await api({
+        method: 'put',
+        url: 'api/envs/enable',
+        params: { t: Date.now() },
+        body: JSON.stringify([eid]),
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+    }).json();
+    return body;
 };
 
-module.exports.getstatus = async(eid) => {
+module.exports.getstatus = async (eid) => {
     const envs = await this.getEnvs();
     var tempid = 0;
     for (let i = 0; i < envs.length; i++) {
-		tempid = 0;
+        tempid = 0;
         if (envs[i]._id) {
             tempid = envs[i]._id;
         }
@@ -158,7 +160,7 @@ module.exports.getstatus = async(eid) => {
     return 99;
 };
 
-module.exports.getEnvById = async(eid) => {
+module.exports.getEnvById = async (eid) => {
     const envs = await this.getEnvs();
     var tempid = 0;
     for (let i = 0; i < envs.length; i++) {
@@ -177,28 +179,28 @@ module.exports.getEnvById = async(eid) => {
 };
 
 module.exports.getEnvByPtPin = async (Ptpin) => {
-  const envs = await this.getEnvs();
-  for (let i = 0; i < envs.length; i++) {	
-	var tempptpin = decodeURIComponent(envs[i].value.match(/pt_pin=([^; ]+)(?=;?)/) && envs[i].value.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-	if(tempptpin==Ptpin){		 
-		 return envs[i]; 
-	  }
-  }  
-  return "";
+    const envs = await this.getEnvs();
+    for (let i = 0; i < envs.length; i++) {
+        var tempptpin = decodeURIComponent(envs[i].value.match(/pt_pin=([^; ]+)(?=;?)/) && envs[i].value.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
+        if (tempptpin == Ptpin) {
+            return envs[i];
+        }
+    }
+    return "";
 };
 
 module.exports.delEnv = async (eid) => {
-  const token = await getToken();
-  const body = await api({
-    method: 'delete',
-    url: 'api/envs',
-    params: { t: Date.now() },
-    body: JSON.stringify([eid]),
-    headers: {
-      Accept: 'application/json',
-      authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  }).json();
-  return body;
+    const token = await getToken();
+    const body = await api({
+        method: 'delete',
+        url: 'api/envs',
+        params: { t: Date.now() },
+        body: JSON.stringify([eid]),
+        headers: {
+            Accept: 'application/json',
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json;charset=UTF-8',
+        },
+    }).json();
+    return body;
 };
